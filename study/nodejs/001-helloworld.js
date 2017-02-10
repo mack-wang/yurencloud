d1a68c57abeba159c802e05ce8476ed18aef6c66
mack-wang <641212003@qq.com>
2017-02-10 20:47:16 +0800

    重新整理了一下文件目录

---
+++ study/nodejs/001-helloworld.js
@@ -0,0 +1,22 @@
+/**
+ * Created by wanglecheng on 2/10/17.
+ */
+
+//在terminal中输入 node 001-helloworld.js 就会输出Hello World Wlc
+console.log('Hello World Wlc');
+
+
+//或者直接node进入nodejs的命令模式，也可以直接执行js命令
+
+/*
+ 如果我们使用PHP来编写后端的代码时，需要Apache 或者 Nginx 的HTTP 服务器，并配上 mod_php5 模块和php-cgi。
+ 从这个角度看，整个"接收 HTTP 请求并提供 Web 页面"的需求根本不需 要 PHP 来处理。
+ 不过对 Node.js 来说，概念完全不一样了。使用 Node.js 时，我们不仅仅 在实现一个应用，同时还实现了整个 HTTP 服务器。
+
+ 引入 required 模块：我们可以使用 require 指令来载入 Node.js 模块。
+ 创建服务器：服务器可以监听客户端的请求，类似于 Apache 、Nginx 等 HTTP 服务器。
+ 接收请求与响应请求 服务器很容易创建，客户端可以使用浏览器或终端发送 HTTP 请求，服务器接收请求后返回响应数据。
+
+*/
+
+
---
+++ study/nodejs/002-require.js
@@ -0,0 +1,8 @@
+/**
+ * Created by wanglecheng on 2/10/17.
+ */
+/*创建 Node.js 应用
+步骤一、引入 required 模块
+我们使用 require 指令来载入 http 模块，并将实例化的 HTTP 赋值给变量 http，实例如下:*/
+var http = require("http");
+
