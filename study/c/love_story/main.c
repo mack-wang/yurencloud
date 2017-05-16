724a2693650c05cfa4bb0cebb7134e1fc89b7a27
mack-wang <641212003@qq.com>
2017-05-16 20:56:37 +0800

    update

--- study/c/love_story/main.c
+++ study/c/love_story/main.c
@@ -1,8 +1,10 @@
 #include "hello.h"
 #include <stdio.h>
+#include <float.h>
 //用引号的是自己的头文件，尖括号的是标准官方的头文件
-int main() {
 
+extern int globals;//定义全局变量，需要加extern
+int main() {
     char boys[] = {'T','B','M'};//在c语言中单引号代表字符（显示为ASCII编码的单个字符），双引号代表字符串
     char boy1, boy2, boy3;//为了让程序的书写更加简洁，C语言支持多个变量的连续定义
     int boy1_age = 21, boy2_age = 22, boy3_age = 34;
@@ -22,6 +24,10 @@ int main() {
     char *key;//key是键(和地址意思相同)   *key是值   &是获取键的地址 key++地址向下移一位 key+8 地址向下移8位
     char value = 'o';
     key = &value;
+
+    int num1 = 123u;//u为unsigned
+    int num2 = 123l;//l为long
+    int num3 = 123ul;
 
 
 
@@ -46,6 +52,34 @@ int main() {
 
     //005-输出指针key的value值
     printf("%c\n",*key);
+
+    //006-获取不同类型的数据的当前机器下的存储大小
+    printf("int 存储大小 : %lu \n", sizeof(int));
+
+    //007-获取浮点数的参数，FLT_MIN等常量声明在float.h头部文件中有
+    printf("float 存储最大字节数 : %lu \n", sizeof(float));
+    printf("float 最小值: %E\n", FLT_MIN );
+    printf("float 最大值: %E\n", FLT_MAX );
+    printf("精度值: %d\n", FLT_DIG );
+
+    //008-定义不同类型的整数
+    printf("%d %d %d\n",num1,num2,num3);//输出的时候并不会出现u和l u和l应该是用来拓展数字的存储空间大小的
+
+    /*转义序列
+                \'	' 字符
+                \"	" 字符
+                \?	? 字符
+                \a	警报铃声
+                \b	退格键
+                \f	换页符
+                \n	换行符
+                \r	回车
+                \t	水平制表符
+                \v	垂直制表符
+                \ooo	一到三位的八进制数
+                \xhh . . .	一个或多个数字的十六进制数
+     */
+
 
     return 0;
 }
