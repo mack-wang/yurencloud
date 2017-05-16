a1268ec7dbb28a97558edabff706c63b6b99f87f
mack-wang <641212003@qq.com>
2017-05-16 23:34:48 +0800

    update 100个C示例学习

---
+++ study/c/100/cmake-build-debug/CMakeFiles/100.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/100.dir/main.c.o"
+  "100.pdb"
+  "100"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/100.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
