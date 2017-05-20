bcf7b5e214d60d2b4eb59b509b69075f7d1942ea
mack-wang <641212003@qq.com>
2017-05-20 15:36:35 +0800

    update

--- study/c/system_c/004-测试和区别大端机器小端机器.c
+++ study/c/system_c/004-测试和区别大端机器小端机器.c
@@ -12,7 +12,9 @@ int main(){
     x0=*((char*)&x); //低地址单元 ,或者((char*)&x)[0];
     x1=*((char*)&x + 1); //高地址单元,或者((char*)&x)[1];
 
-    printf("x0=%x\nx1=%x\n",x0,x1);
-
+    if(x0 == 0x11)
+        printf("我是大端机器");
+    if(x0 == 0x22)
+        printf("我是小端机器");
     return 0;
 }
