87900aca3276f6e5114fa7376608c596250b156e
mack-wang <641212003@qq.com>
2017-07-20 11:26:21 +0800

    回文的算法做好了，但是性能不èå好

--- study/javascript/leetCode/005-longest-palindromic.js
+++ study/javascript/leetCode/005-longest-palindromic.js
@@ -2,24 +2,30 @@
  * @param {string} s
  * @return {number}
  */
-var longestPalindrome = function(s) {
-    var longest='', temp = '', i, j;
-    for(i=0;i<s.length;i++){
-        for(j=i;j<s.length;j++){
-            if(temp[0] === s[j]){
-                if(longest.length<temp.length+1){
-                    temp += s[j];
-                    longest=temp;
+var longestPalindrome = function (s) {
+    var max = 0, judge = true, i, j, k, len = s.length, start, section;
+
+    if (len === 1) return s;
+
+    for (i = 0; i < len; i++) {
+        for (k = len; k > i; k--) {
+            section = k - i;
+            for (j = 0; j < (0 | (section / 2)); j++) {
+                if (s[i + j] !== s[k - j - 1]) {
+                    judge = false;
+                    break;
                 }
-                temp = '';
-                break;
-            }else{
-                temp += s[j];
             }
+            if (judge && max < section) {
+                max = section;
+                start = i;
+            }
+            judge = true;
         }
-        temp = '';
     }
-    console.log(longest);
+    return s.substring(start, start + max);
 };
 
-longestPalindrome("pwadwkedsw");
+console.log(
+    longestPalindrome("pwwppwwp")
+);
